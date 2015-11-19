0f027136d483d2f5a75619be000a41c70e20ed46
tiou <teoraunio@hotmail.com>
2015-11-19 14:49:03 +0200

    back to html

---
+++ search.php
@@ -0,0 +1,27 @@
+		<?php
+		$servername = "vituttaa.soikkelionhomo.pw";
+		$username = "parina";
+		$password = "sciparina";
+		$dbname = "sciparina";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+     		die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "SELECT nimi, rahat, voitot, pelatut_pelit FROM kayttaja";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+     		// output data of each row
+     		while($row = $result->fetch_assoc()) {
+        		echo "<br> Name: ". $row["nimi"]. " - Money: ". $row["rahat"]. " - Winnings: " . $row["voitot"] . " - Games Played ". $row["pelatut_pelit"] . " <br>";
+     		}
+		} else {
+     		echo "No results";
+		}
+
+		$conn->close();
+		?>  
